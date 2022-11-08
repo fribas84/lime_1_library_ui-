c38116dd3d27634fafb72d60c0e12cce1d7acb56
fribas84 <fribas84.gmail.com>
2022-11-08 20:38:35 -0300

    pointing to goerli deployment

--- constants/index.ts
+++ constants/index.ts
@@ -14,6 +14,6 @@
   export const supportedMetamaskNetworks = [1, 3, 4, 5, 42,31337];
 
   export const ALBT_TOKEN_ADDRESS = "0xc6869a93ef55e1d8ec8fdcda89c9d93616cf0a72";
-  export const US_ELECTION_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+  export const US_ELECTION_ADDRESS = "0x623B29a6BE7e88211EDd718546b3AA03386C489b";
   //export const US_ELECTION_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   
